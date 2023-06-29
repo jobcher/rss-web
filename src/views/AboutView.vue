76e0f34289c21b85fb37d221fe8b0c38831c805a
jobcher <13028911306@163.com>
2023-06-29 17:14:42 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-main class="main">
+        <h1>About</h1>
+        <p>This is the about page</p>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.common-layout {
+  height: 100%;
+  width: 100%;
+  background-color: #f2f2f2;
+  margin-top: 20px;
+}
+
+.main {
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.12), 0 0 6px 0 rgba(0,0,0,.04);
+  border-radius: 5px;
+}
+</style>
